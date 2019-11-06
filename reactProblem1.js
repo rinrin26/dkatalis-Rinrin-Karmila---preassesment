a730c20f3ba64107adf10bea3350b40e2f7e677a
rinrin26 <56067805+rinrin26@users.noreply.github.com>
2019-11-06 13:56:16 +0700

    ReactProblem1

---
+++ reactProblem1.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+import {View,Textinput } from 'react';
+render(){
+    return(
+        <View>
+            <Textinput> nama </Textinput>
+        </View>
+    )
+    }
+    
